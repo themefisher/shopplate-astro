4db2f146a45737ee32f754c9f9e03ae9a570309e
Somrat <58769763+tfsomrat@users.noreply.github.com>
2025-12-01 13:55:15 +0600

    remove custom tailwind plugins and added a script to generate theme.css

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,13 @@
 import mdx from "@astrojs/mdx";
+import netlify from "@astrojs/netlify";
 import react from "@astrojs/react";
 import sitemap from "@astrojs/sitemap";
-import netlify from "@astrojs/netlify";
 import tailwindcss from "@tailwindcss/vite";
 import AutoImport from "astro-auto-import";
 import { defineConfig } from "astro/config";
 import remarkCollapse from "remark-collapse";
 import remarkToc from "remark-toc";
+import sharp from "sharp";
 import config from "./src/config/config.json";
 
 // https://astro.build/config
@@ -14,6 +15,7 @@ export default defineConfig({
   site: config.site.base_url ? config.site.base_url : "http://examplesite.com",
   base: config.site.base_path ? config.site.base_path : "/",
   trailingSlash: config.site.trailing_slash ? "always" : "never",
+  image: { service: sharp() },
   output: "server",
   adapter: netlify({
     edgeMiddleware: true,
@@ -35,7 +37,6 @@ export default defineConfig({
     }),
     mdx(),
   ],
-
   markdown: {
     remarkPlugins: [remarkToc, [remarkCollapse, { test: "Table of contents" }]],
     shikiConfig: { theme: "one-dark-pro", wrap: true },
